85cbc914524d44c64a52043df528c1acab4897c0
vineetsingh0809 <ankitchilma@gmail.com>
2023-07-13 18:20:13 +0530

    fixing styling issue

--- src/components/Subscriber/SubscriberBadge.jsx
+++ src/components/Subscriber/SubscriberBadge.jsx
@@ -4,62 +4,62 @@ import React, { useState } from "react";
 const badgesData = [
   {
     id: 1,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://images.freeimages.com/images/large-previews/928/austin-healey-badge-1488054.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 2,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2018/04/06/13/46/poly-3295856_1280.png",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 3,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2018/04/09/19/55/low-poly-3305284_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 4,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2018/04/11/11/54/small-poly-3310319_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 5,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2012/04/26/13/12/star-42467_1280.png",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 6,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2018/04/11/11/55/small-poly-3310321_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 7,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2018/04/25/18/08/small-poly-3350170_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 8,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2018/03/12/15/06/deer-3219872_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 9,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2015/12/09/16/32/badge-1084984_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 10,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2016/11/30/18/14/like-1873541_1280.png",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 11,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2020/07/10/02/45/tattoo-5389284_1280.png",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
   {
     id: 12,
-    url: "https://yt3.ggpht.com/ytc/AGIKgqOTuwiukfCtfTL4W2mn28cIowlryUV0gTHt7wuA=s48-c-k-c0x00ffffff-no-rj",
+    url: "https://cdn.pixabay.com/photo/2016/12/13/22/39/motorcycle-1905281_1280.jpg",
     gif: "https://media.tenor.com/P-8ZvqnS4AwAAAAM/dancing-cat-dancing-kitten.gif",
   },
 ];
@@ -77,7 +77,7 @@ const SubscriberBadge = () => {
 
   return (
     <div className="mt-5">
-      <h2 className="ml-5 text-white text-xl">
+      <h2 className="ml-10 text-white text-xl">
         12 Custom Emotes for comments and chat
       </h2>
       <div className="flex items-center justify-center flex-wrap max-w-md">
